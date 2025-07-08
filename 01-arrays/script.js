22f70aa3b8b49bc18a553ffba3b3bc18d14ee734
KWAshen <ashen.maliaha@gmail.com>
2025-07-08 22:51:09 +0200

    fine programma

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -25,17 +25,33 @@ console.log(teachers)
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
 console.log(firstTeacher);
-console.log(teacehers);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
 console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-
+teachers.push('Sara');
+console.log(teachers);
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let result;
+if(teachers.length > 0){
+  rusult = false;
+}
+else{
+  result = true;
+}
+if(teachers.length){
+  console.log(true)
+}
+else{
+  console.log(false);
+}
+const isTeachersEmpty = result;
+console.log(isTeachersEmpty);
